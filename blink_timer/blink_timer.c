30b151faa8376f288bd62b64eed2dabd0d2c7a02
Yevhenii Popravka <yevhenii.popravka@gmail.com>
2019-03-18 23:52:57 +0200

    it build but don't blink
    check timer clock

--- blink_timer/blink_timer.c
+++ blink_timer/blink_timer.c
@@ -1,33 +1,32 @@
 #include "stdint.h"
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include "delay_tim6.h"
 
 #define LED_PORT RCC_GPIOC
 #define LED_PIN GPIO13
 
 int main(void) {
 
-	/* Enable clock for GPIO port C */
-	rcc_periph_clock_enable(RCC_GPIOC);
+    /* Enable clock for GPIO port C */
+    rcc_periph_clock_enable(RCC_GPIOC);
 
-	/* Set to run tim6 */
-	delay_setup();
+    /* Set to run tim6 */
+    delay_setup();
 
-	/* Set pin to push-pull configuration */
-	gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
+    /* Set pin to push-pull configuration */
+    gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
 
-	/* Init port to "1" (LED off) */
-	gpio_set(GPIOC, GPIO13);
+    /* Init port to "1" (LED off) */
+    gpio_set(GPIOC, GPIO13);
 
-	while (1u) {
-		/* Wait a bit */
-		delay_us(300u);
+    while (1u) {
+        /* Wait a bit */
+        delay_us(300000u);
 
-		/* Change the state of the LED */
-		gpio_toggle(GPIOC, GPIO13);
-
-
-	}
+        /* Change the state of the LED */
+        gpio_toggle(GPIOC, GPIO13);
+    }
 
 }
 
--- blink_timer/delay_tim6.c
+++ blink_timer/delay_tim6.c
@@ -4,18 +4,18 @@
 #include "delay_tim6.h"
 
 void delay_setup(void) {
-	/* set up a microsecond free running timer for ... things... */
-	rcc_periph_clock_enable(RCC_TIM6);
-	/* microsecond counter */
-	timer_set_prescaler(TIM6, rcc_apb1_frequency / 1e6 - 1);
-	timer_set_period(TIM6, 0xffff);
-	timer_one_shot_mode(TIM6);
+    /* set up a microsecond free running timer for ... things... */
+    rcc_periph_clock_enable(RCC_TIM6);
+    /* microsecond counter */
+    timer_set_prescaler(TIM6, rcc_apb1_frequency / 1e6 - 1);
+    timer_set_period(TIM6, 0xffff);
+    timer_one_shot_mode(TIM6);
 }
 
 void delay_us(uint16_t us) {
-	TIM_ARR(TIM6) = us;
-	TIM_EGR(TIM6) = TIM_EGR_UG;
-	TIM_CR1(TIM6) |= TIM_CR1_CEN;
-	//timer_enable_counter(TIM6);
-	while (TIM_CR1(TIM6) & TIM_CR1_CEN);
+    TIM_ARR(TIM6) = us;
+    TIM_EGR(TIM6) = TIM_EGR_UG;
+    TIM_CR1(TIM6) |= TIM_CR1_CEN;
+    //timer_enable_counter(TIM6);
+    while (TIM_CR1(TIM6) & TIM_CR1_CEN);
 }
